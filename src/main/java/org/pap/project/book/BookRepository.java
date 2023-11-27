87d707f1a3b988b98849c075587aa0c73a783efa
Andrzej Tokajuk <atokajuk@match-trade.com>
2023-11-27 21:16:07 +0100

    book handling

---
+++ src/main/java/org/pap/project/book/BookRepository.java
@@ -0,0 +1,10 @@
+package org.pap.project.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+
+}
+
---
+++ src/main/java/org/pap/project/book/BookService.java
@@ -0,0 +1,40 @@
+package org.pap.project.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/book")
+    public Optional<Book> getBookById(@RequestParam(value="isbn")String isbn) {
+        return bookRepository.findById(isbn);
+    }
+
+    @PostMapping("/book")
+    public void addNewBook(@RequestBody Book book) {
+        Optional<Book> bookOptional = bookRepository.findById(book.getIsbn());
+        if (bookOptional.isPresent()) {
+            throw new IllegalStateException("Book already exists");
+        }
+        bookRepository.save(book);
+
+    }
+}
