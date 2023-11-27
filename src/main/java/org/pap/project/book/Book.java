8cd56e4af46476e69bd0602b6b4c2f486c0203d5
Andrzej Tokajuk <atokajuk@match-trade.com>
2023-11-27 21:16:07 +0100

    book handling

---
+++ src/main/java/org/pap/project/book/Book.java
@@ -0,0 +1,27 @@
+package org.pap.project.book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+public class Book {
+    @Id
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+}
---
+++ src/main/java/org/pap/project/book/BookController.java
@@ -0,0 +1,9 @@
+package org.pap.project.book;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+
+}
