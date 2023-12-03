3b2ab41f026d2f7073ab509a3c2fee5dee851346
afilewic <01178522@pw.edu.pl>
2023-12-03 20:08:31 +0100

    added your network connection

--- backend/src/main/java/org/pap/project/config/WebMvcConfig.java
+++ backend/src/main/java/org/pap/project/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/books")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.169:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
