5b33c2a10125b8e9dde0508fcc919760c39df3d4
Andrzej Tokajuk <atokajuk@match-trade.com>
2023-12-27 13:25:48 +0100

    changes

--- frontend/src/BookList.jsx
+++ frontend/src/BookList.jsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import AddBookForm from './AddBookForm';
-import DeleteBookForm from './DeleteBookForm';
+import React, { Component } from "react";
+import AddBookForm from "./AddBookForm";
+import DeleteBookForm from "./DeleteBookForm";
 
 class BookList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      books: [],
-      isAddFormVisible: false,
-      isDeleteFormVisible: false,
-    };
-  }
+  state = {
+    books: [],
+    isAddFormVisible: false,
+    isDeleteFormVisible: false,
+  };
 
   componentDidMount() {
     this.fetchBooks();
   }
 
   fetchBooks = () => {
-    fetch('http://localhost:8080/books')
+    fetch("http://localhost:8080/books")
       .then((response) => response.json())
       .then((data) => this.setState({ books: data }))
-      .catch((error) => console.error('Error fetching book data:', error));
+      .catch((error) => console.error("Error fetching book data:", error));
   };
 
   addBook = (newBook) => {
@@ -39,20 +36,6 @@ class BookList extends Component {
     }));
   };
 
-  renderBook = (book) => (
-    <li key={book.isbn}>
-      <div>
-        <h2>
-          <p>{book.title}</p>
-        </h2>
-      </div>
-      <p className="author">Author: {book.author}</p>
-      <p>isbn: {book.isbn}</p>
-    </li>
-  );
-
-  renderBookList = () => <ul>{this.state.books.map(this.renderBook)}</ul>;
-
   handleToggleForm = (form) => {
     this.setState((prevState) => ({
       [form]: !prevState[form],
@@ -61,19 +44,37 @@ class BookList extends Component {
 
   render() {
     return (
-      <div className="BookList">
-        {this.renderBookList()}
-        <button onClick={() => this.handleToggleForm('isAddFormVisible')}>
+      <React.Fragment>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>ISBN</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.books.map((book) => (
+              <tr key={book.isbn}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.isbn}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <button onClick={() => this.handleToggleForm("isAddFormVisible")}>
           Add Book
         </button>
         {this.state.isAddFormVisible && <AddBookForm addBook={this.addBook} />}
-        <button onClick={() => this.handleToggleForm('isDeleteFormVisible')}>
+        <button onClick={() => this.handleToggleForm("isDeleteFormVisible")}>
           Delete Book
         </button>
         {this.state.isDeleteFormVisible && (
           <DeleteBookForm deleteBook={this.deleteBook} />
         )}
-      </div>
+      </React.Fragment>
     );
   }
 }
