3a9f6f2513e655a7e233d6d4eb7c7c79d0663c4e
Andrzej Tokajuk <atokajuk@match-trade.com>
2024-01-01 20:34:29 +0100

    added navbar

---
+++ frontend/src/components/navBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <NavLink className="navbar-brand" to="/">
+        Library
+      </NavLink>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/addbookform">
+            Add
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/deletebookform">
+            Delete
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/login">
+            Login
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
